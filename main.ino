67d16a623c4ebf93dd04e215b011cbec6232aaae
MCS51 <armcortexfpga@gmail.com>
2020-06-17 23:02:19 +0800

    1. add LED indicator and re-calc servoOff() 2. servoOn(), servoOff() different timing

--- main.ino
+++ main.ino
@@ -3,15 +3,12 @@
 #include <SonarSRF02.h>
 #include <Timer.h>
 
-    // "sketch": "driver/Arduino-SRF/examples/SRF02/SRF02.ino"
-
-
-// Define
+// "sketch": "driver/Arduino-SRF/examples/SRF02/SRF02.ino"
 
 // SRF02 Parameters
 #define SONAR_ADDRESS   (0xE0 >> 1)
 #define SONAR_RANGE_MIN (22)
-#define SONAR_RANGE_MAX (60)
+#define SONAR_RANGE_MAX (150)
 #define SONAR_FILTER    (0.7)
 
 SonarSRF02 Sonar(SONAR_ADDRESS);
@@ -35,65 +32,41 @@ unsigned int servo_angle = 0;
 bool servo_state = false;
 
 // Timer
+#define OFF_TIME      (65536)
 Timer t;
+char servoOff_id = 0;
 
+// Other
+unsigned int cnt = 0;
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   servo.attach(SERVO_PIN);
+  servo.write(SERVO_OFF);
   Sonar.begin();
 
   t.every(10, print);
   t.every(10, readADC);
   t.every(10, readSonar);
-  t.every(3000, controlServo);
+  t.every(300, servoOn);
+  servoOff_id = t.every(OFF_TIME, servoOff);
 }
 
 void loop() {
-  t.update();
+  t.update();    
 
-
-
-  // pot = (ADC_FILTER * pot) + ((1-ADC_FILTER) * analogRead(POT_PIN));
-  // sonar_dis = (SONAR_FILTER * sonar_dis) + ((1-SONAR_FILTER) * Sonar.readRange(sonar_unit));
-
-  // sonar_thres = map(pot, 0, 1024, SONAR_RANGE_MIN, SONAR_RANGE_MAX);
-
-  // if (sonar_dis > sonar_thres)
-  // {
-  //   servo.write(SERVO_ON);
-  //   servo_state = true;
-  // }
-  // else
-  // {
-  //   servo.write(SERVO_OFF);
-  //   servo_state = false;
-  // }
-  
-  // Serial.print("Threshold: ");
-  // Serial.print(sonar_thres);
-  // Serial.print(" Sonar: ");
-  // Serial.print(Sonar.readRange(sonar_unit));
-  // Serial.print(" cm ");
-  // Serial.print("Servo: ");
-  // Serial.print(servo_state);
-  // Serial.println();
-
-  // delay(10);
-
-
-  // // servo_angle = map(pot, 0, 1024, 0, 180);
-
-
-  // // servo.write(servo_angle);
-
-  // // // digitalWrite(LED_BUILTIN, LOW); 
-  // // Serial.print("pot: ");
-  // // Serial.print(pot);
-  // // Serial.print(" angle: ");
-  // // Serial.println(servo_angle);
-  // // delay(10);                       
+  if (sonar_dis < sonar_thres)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    // re-calculate
+    t.stop(servoOff_id);
+    servoOff_id = t.every(OFF_TIME, servoOff);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
 unsigned int filter(unsigned int oldValue, unsigned int newValue, float n)
@@ -104,27 +77,26 @@ unsigned int filter(unsigned int oldValue, unsigned int newValue, float n)
 void readADC()
 {
   pot = filter(pot, analogRead(POT_PIN), ADC_FILTER);
-
-  // pot = (ADC_FILTER * pot) + ((1-ADC_FILTER) * analogRead(POT_PIN));
   sonar_thres = map(pot, 0, 1024, SONAR_RANGE_MIN, SONAR_RANGE_MAX);
 }
 
 void readSonar()
 {
-  // sonar_dis = (SONAR_FILTER * sonar_dis) + ((1-SONAR_FILTER) * Sonar.readRange(sonar_unit));
-  // sonar_dis = Sonar.readRange(sonar_unit);
-
   sonar_dis = filter(sonar_dis, Sonar.readRange(sonar_unit), SONAR_FILTER);
 }
 
-void controlServo()
+void servoOn()
 {
   if (sonar_dis < sonar_thres)
   {
     servo.write(SERVO_ON);
     servo_state = true;
   }
-  else
+}
+
+void servoOff()
+{
+  if (sonar_dis >= sonar_thres)
   {
     servo.write(SERVO_OFF);
     servo_state = false;
@@ -133,12 +105,17 @@ void controlServo()
 
 void print()
 {
-  Serial.print("Threshold: ");
+  Serial.print(cnt);
+  Serial.print(". Threshold: ");
   Serial.print(sonar_thres);
   Serial.print(" Sonar: ");
   Serial.print(sonar_dis);
-  Serial.print(" cm ");
-  Serial.print("Servo: ");
+  Serial.print(" cm");
+  Serial.print(" On: ");
+  Serial.print(sonar_dis < sonar_thres);
+  Serial.print(" Servo: ");
   Serial.print(servo_state);
+
   Serial.println();
+  cnt++;
 }
